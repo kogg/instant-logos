2c67383a91c438c6831605fabbf16db4cc87d3e4
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-26 00:51:54 -0800

    Fill with dummy data

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = [
+	{ id: 0, name: 'adobe illustrator' },
+	{ id: 1, name: 'adobe photoshop' },
+	{ id: 2, name: 'adobe' },
+	{ id: 3, name: 'facebook' },
+	{ id: 4, name: 'netflix' },
+	{ id: 5, name: 'redfin' },
+	{ id: 6, name: 'zillow' },
+];
