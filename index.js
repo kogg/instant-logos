10c544144223f6cb3d75682131a0d7359e252202
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-28 12:13:54 -0800

    more data

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 module.exports = [
-	{ id: 0, name: 'Adobe' },
-	{ id: 1, name: 'Adobe Illustrator' },
-	{ id: 2, name: 'Adobe Photoshop' },
-	{ id: 3, name: 'Facebook' },
-	{ id: 4, name: 'Netflix' },
-	{ id: 5, name: 'Redfin' },
-	{ id: 6, name: 'Zillow' },
+	{ id: 0, name: 'Adobe',             svg_url: 'not-a-svg.svg', png_url: 'not-a-png.png' },
+	{ id: 1, name: 'Adobe Illustrator', svg_url: 'not-a-svg.svg', png_url: 'not-a-png.png' },
+	{ id: 2, name: 'Adobe Photoshop',   svg_url: 'not-a-svg.svg', png_url: 'not-a-png.png' },
+	{ id: 3, name: 'Facebook',          svg_url: 'not-a-svg.svg', png_url: 'not-a-png.png' },
+	{ id: 4, name: 'Netflix',           svg_url: 'not-a-svg.svg', png_url: 'not-a-png.png' },
+	{ id: 5, name: 'Redfin',            svg_url: 'not-a-svg.svg', png_url: 'not-a-png.png' },
+	{ id: 6, name: 'Zillow',            svg_url: 'not-a-svg.svg', png_url: 'not-a-png.png' },
 ];
