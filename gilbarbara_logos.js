0e0db2716fc772efcdee6ac7721c53f9e48afd3c
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-01-31 03:04:07 -0800

    using speakingurl for file logo slugs

--- gilbarbara_logos.js
+++ gilbarbara_logos.js
@@ -2,25 +2,27 @@ var _                = require('underscore');
 var gilbarbara_logos = require('logos/app/logos');
 var path             = require('path');
 
+var source = {
+	name:      'SVG PORN',
+	shortname: 'svgporn'
+};
+
 module.exports = _.chain(gilbarbara_logos)
 	.result('items')
 	.map(function(logo) {
 		return _.map(logo.files, function(file, i) {
 			return {
-				id:   'svg-porn-' + logo.shortname + '-' + i,
-				name: logo.name,
-				svg:  {
+				id:     source.shortname + '-' + logo.shortname + '-' + i,
+				name:   logo.name,
+				source: source,
+				svg:    {
 					path: {
 						directory: path.join(__dirname, 'node_modules/logos/logos'),
 						filename:  file
 					}
-				},
-				source: {
-					name:      'SVG PORN',
-					shortname: 'svgporn'
 				}
 			};
 		});
 	})
-	.flatten()
+	.flatten(true)
 	.value();
