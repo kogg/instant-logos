b06501299eededb61aa4da17fb798e6ea50103ee
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-02 15:40:04 -0800

    Some fixes

--- andyfitz_logomono.js
+++ andyfitz_logomono.js
@@ -3,6 +3,11 @@ var fs       = require('fs');
 var getSlug  = require('speakingurl');
 var path     = require('path');
 
+var replacements = {
+	Googleplusicon: 'Google Plus',
+	Googlewallet:   'Google Wallet'
+};
+
 module.exports = _.chain(fs.readdirSync(path.join(__dirname, 'node_modules/andyfitz_logomono/logos')))
 	.without('heat.svg', 'helpdesk.svg', 'inkscape.svg')
 	.filter(function(file) {
@@ -10,12 +15,13 @@ module.exports = _.chain(fs.readdirSync(path.join(__dirname, 'node_modules/andyf
 	})
 	.map(function(file) {
 		file = file.replace(/\.svg$/, '');
-		var name = getSlug(file.replace(/[\-_](?:badge|icon|wordmark|(?:no)?logomark)\|punchout/, ''), {
+		var name = getSlug(file.replace(/[\-_](?:badge|icon|wordmark|(?:no)?logomark\|punchout)/, ''), {
 			separator: ' ',
 			titleCase: ['and', 'of', 'the'],
 			custom:    { '-': ' ', '_': ' ' }
 		});
 		name = name.charAt(0).toUpperCase() + name.slice(1);
+		name = replacements[name] || name;
 		return {
 			id:          getSlug(name + ' mono'),
 			name:        name,
