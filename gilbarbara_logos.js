674565bd702089878879d2c4a8e0fc9d269595b6
Saiichi Hashimoto <saiichihashimoto@gmail.com>
2016-02-02 15:01:52 -0800

    noisy keywords from svgporn

--- gilbarbara_logos.js
+++ gilbarbara_logos.js
@@ -16,7 +16,7 @@ module.exports = _.chain(gilbarbara_logos)
 			return {
 				id:          logo.shortname,
 				name:        logo.name,
-				keywords:    _.chain(logo.tags).union(logo.categories).invoke('toLowerCase').sort().value(),
+				keywords:    [],
 				contributor: {
 					name:      'Gil Barbara',
 					shortname: 'gilbarbara'
